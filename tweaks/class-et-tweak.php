93e5f82c0ed801aeb5aeb85f1aaf234b661b47b5
Dustin Falgout <dustin@falgout.us>
2016-05-08 19:50:55 -0500

    Update class-et-tweak.php

--- tweaks/class-et-tweak.php
+++ tweaks/class-et-tweak.php
@@ -3,6 +3,7 @@
 class My_ET_Tweak {
 	public $builder_module_tweak = false;
 	public $builder_module_class = '';
+	public $builder_module = null;
 
 	public function __construct() {
 		if ( false === $this->builder_module_tweak ) {
@@ -14,7 +15,15 @@ class My_ET_Tweak {
 	
 	public function apply_tweak() {}
 	
-	public function apply_builder_module_tweak() {}
+	public function apply_builder_module_tweak() {
+		if ( class_exists( $this->builder_module_class ) ) {
+			$builder_module = 'My_' . $this->builder_module_class;
+			$this->builder_module = new $builder_module;
+			
+			remove_shortcode( $this->builder_module->slug );
+			add_shortcode( $this->builder_module->slug, array( $this->builder_module, '_shortcode_callback' );
+		}
+	}
 	
 }
 
